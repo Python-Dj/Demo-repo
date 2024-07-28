1b527cc1168f77c920cd6561be45cddcefd80a3c
Suraj gupta <agentgupta216@gmail.com>
2024-07-28 20:19:51 +0530

    added a book class in aap.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 print("hello git from my side")
+
+for i in range(10):
+    print(i)
+
+class Books:
+    def __init__(self, book_name):
+        self.book_name = book_name
+
+    def __repr__(self):
+        return self.book_name
+    
